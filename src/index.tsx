e8243b1ff0f044e424789f8b0fced41d3a99f0ec
Colton Ehrman <coltonje95@gmail.com>
2023-12-18 18:52:18 -0600

    fix(router): browser router to use new base path

--- src/index.tsx
+++ src/index.tsx
@@ -2,28 +2,21 @@ import CssBaseline from "@mui/joy/CssBaseline";
 import { CssVarsProvider } from "@mui/joy/styles";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 import "@fontsource/inter";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const node = (document.getElementById("root") ?? document.body) as Element;
 
 ReactDOM.createRoot(node).render(
-  <RouterProvider
-    router={createBrowserRouter([
-      {
-        path: "*",
-        element: (
-          <CssVarsProvider>
-            {/* must be used under CssVarsProvider */}
-            <CssBaseline />
-            <App />
+  <BrowserRouter basename="/budget-app">
+    <CssVarsProvider>
+      {/* must be used under CssVarsProvider */}
+      <CssBaseline />
+      <App />
 
-            {/* The rest of your application */}
-          </CssVarsProvider>
-        ),
-      },
-    ])}
-  />,
+      {/* The rest of your application */}
+    </CssVarsProvider>
+  </BrowserRouter>,
 );
