bef71197700e002c5fbc0d728f368eef77c1ea95
Colton Ehrman <coltonje95@gmail.com>
2024-01-07 10:45:21 -0600

    feat: add https

--- prod-server.js
+++ prod-server.js
@@ -1,11 +1,23 @@
+import fs from "fs";
+import http from "http";
+import https from "https";
 import express from "express";
 
 const app = express();
-const port = 80;
 
 app.use("/budget-app", express.static("dist"));
 app.use("/budget-app", express.static("build"));
 
-app.listen(port, () => {
-  // console.log(`Example app listening on port ${port}`);
-});
+let httpsServer;
+
+if (process.env.HTTPS_PRIVATE_KEY && process.env.HTTPS_CERT) {
+  const key = fs.readFileSync(process.env.HTTPS_PRIVATE_KEY, "utf8");
+  const cert = fs.readFileSync(process.env.HTTPS_CERT, "utf8");
+
+  httpsServer = https.createServer({ key, cert }, app);
+}
+
+const httpServer = http.createServer(app);
+
+httpServer.listen(80);
+httpsServer.listen(443);
