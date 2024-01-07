16a218269595d8eb5690513ab29f3fd45f895889
Colton Ehrman <coltonje95@gmail.com>
2024-01-06 22:40:49 -0600

    feat: update for production

---
+++ prod-server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 80;
+
+app.use("/budget-app", express.static("dist"));
+app.use("/budget-app", express.static("build"));
+
+app.listen(port, () => {
+  // console.log(`Example app listening on port ${port}`);
+});
