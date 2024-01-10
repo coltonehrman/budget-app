0ede72e7cc1fc423f09a4a814d54f3092b1f4cc6
Colton Ehrman <coltonje95@gmail.com>
2024-01-09 19:51:14 -0600

    refactor: remove SSL config in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/budget-app/",
-  plugins: [basicSsl(), react()],
+  plugins: [react()],
 });
